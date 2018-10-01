c7cb5f42e230758a7e8586d1ee798b1a6d7c6fcd
kistea1 <aaron.e.kistenmacher@medtronic.com>
2018-10-01 09:52:35 -0600

    Simple mouse handling

--- HelloCanvas.js
+++ HelloCanvas.js
@@ -1,7 +1,9 @@
 const VSHADER_SOURCE = `
+  attribute vec4 aPosition;
+  attribute float aPointSize;
   void main() {
-    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-    gl_PointSize = 10.0;
+    gl_Position = aPosition;
+    gl_PointSize = aPointSize;
   }
 `
 
@@ -53,10 +55,39 @@ function main() {
     return;
   }
 
-  gl.useProgram(initShaderProgram(gl, VSHADER_SOURCE, FSHADER_SOURCE));
+  const program = initShaderProgram(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+  gl.useProgram(program);
+
+  var aPosition = gl.getAttribLocation(program, 'aPosition');
+  var aPointSize = gl.getAttribLocation(program, 'aPointSize');
+
+  gl.vertexAttrib1f(aPointSize, 5.0);
+
+  canvas.onmousedown = function (event) { click (event, gl, canvas, aPosition); };
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   gl.drawArrays(gl.POINTS, 0, 1);
+}
+
+var gPoints = [];
+function click (event, gl, canvas, aPosition) {
+  var x = event.clientX;
+  var y = event.clientY;
+  var rect = event.target.getBoundingClientRect();
+
+  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+
+  gPoints.push({ x: x, y: y });
+
+  // gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var len = gPoints.length;
+
+  for (var i = 0; i < len; i++) {
+    gl.vertexAttrib3f(aPosition, gPoints[i].x, gPoints[i].y, 0.0);
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
 }
